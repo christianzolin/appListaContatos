7dbf6756504582e67d444349cf0753d48943a9ca
christianzolin <christian.koffer@gmail.com>
2020-05-15 00:34:27 -0300

    adicionando avatar

--- src/compoents/ListaPessoaItens.js
+++ src/compoents/ListaPessoaItens.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Image } from 'react-native'
 
 const ListaPessoaItens = props => {
     const { pessoa } = props;
     const { first, last } = pessoa.name
+    const { thumbnail } = pessoa.picture
     return (
         <View style={styles.line}>
+            <Image source={{uri: thumbnail }} style={styles.avatar}/>
             <Text style={styles.lineText}> {`${first} ${last} `}</Text>
         </View>
     )
@@ -13,7 +15,7 @@ const ListaPessoaItens = props => {
 
 const styles = StyleSheet.create({
     line: {
-        height: 50,
+        height: 60,
         borderBottomWidth: 1,
         borderBottomColor: "#bbb",
         flexDirection: 'row',
@@ -21,7 +23,14 @@ const styles = StyleSheet.create({
     },
     lineText: {
         fontSize: 20,
-        paddingLeft: 1,
+        paddingLeft: 10,
+        flex: 7
+    },
+    avatar:{
+        aspectRatio: 1,
+        flex: 1,
+        marginLeft: 5,
+        borderRadius: 50
     }
 })
 export default ListaPessoaItens;
